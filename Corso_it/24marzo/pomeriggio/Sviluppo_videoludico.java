00309aa08ae088301ee7b772501ad4efb3a73593
DonatoMorra <donatomorra90@gmail.com>
2026-03-24 17:58:32 +0100

    Update Sviluppo_videoludico.java

--- Corso_it/24marzo/pomeriggio/Sviluppo_videoludico.java
+++ Corso_it/24marzo/pomeriggio/Sviluppo_videoludico.java
@@ -11,6 +11,8 @@ public class Sviluppo_videoludico {
         Team team2 = new Team("Team 2");
         Team team3  = new Team("Team 3");
 
+        
+        
         boolean continua = true;
 
         while (continua) {
@@ -32,6 +34,8 @@ public class Sviluppo_videoludico {
                     System.out.print("Scegli il team (1-3): ");
                     int tem = myint.nextInt();
                     
+                    System.out.println("Inserisci nome sviluppatore");
+                    String nomsvil=mystr.nextLine();
 
                     System.out.print("Inserisci Titolo: ");
                     String titolo = mystr.nextLine();
@@ -42,7 +46,7 @@ public class Sviluppo_videoludico {
                     System.out.print("Inserisci costo sviluppo: ");
                     int csv = myint.nextInt();
 
-                    System.out.println("Dimmi stato del progetto");
+                    System.out.println("Dimmi stato del progetto (IN SVILUPPO , IN TEST, PUBLICATO)");
                     String stp=mystr.nextLine();
                     
 
@@ -66,19 +70,118 @@ public class Sviluppo_videoludico {
                     break;
 
                 case 2:
-                    team1.stampaNegozio();
-                    team2.stampaNegozio();
-                    team3.stampaNegozio();
+
+                    
+                    
+
+                    if(team1.gioco == null)
+                    {
+                        System.out.println("Team1 non ha giochi inseriti");
+                        break;
+                    }
+                    else
+                    {
+                        team1.stampaTeam();
+                    
+                    }
+
+                     if(team2.gioco == null)
+                    {
+                        System.out.println("Team non esistente");
+                        break;
+                    }
+                    else
+                    {
+                        team2.stampaTeam();
+                        
+                    }
+
+                     if(team3.gioco==null)
+                    {
+                        System.out.println("Team non esistente");
+                        break;
+                    }
+                    else
+                    {
+                         team3.stampaTeam();
+                    }
                     break;
 
                 case 3:
+                        
+                    Gioco giocomax=null;
+                    
+                    if (team1.gioco != null) {
+                        giocomax = team1.gioco;
+                    }
+
+                    
+                    if (team2.gioco != null) {
+                        if (team2.gioco.costo_sviluppo > giocomax.costo_sviluppo) {
+                            giocomax = team2.gioco;
+                        }
+                    }
+
+                    
+                    if (team3.gioco != null) {
+                        if (team3.gioco.costo_sviluppo > giocomax.costo_sviluppo) {
+                            giocomax = team3.gioco;
+                        }
+                    }
+
+                    if (giocomax != null) {
+                        System.out.println("Il gioco con il costo più alto è:");
+                        giocomax.stampaGioco();
+                    } else {
+                        System.out.println("Nessun gioco inserito");
+                    }
+
+                    break;
+                case 4:
+
+                    System.out.println("Quale team vuoi modificare lo stato del gioco (1-team1 2-team2 3-team3)");
+                    int mod=myint.nextInt();
+                    
                     
 
-                case 4:
+                    switch (mod) {
+                        case 1:
+                                System.out.println("Dimmi stato del progetto (IN SVILUPPO , IN TEST, PUBLICATO)");
+                                String nuovostato=mystr.nextLine();
+                                nuovostato= team1.gioco.stato_del_progetto;
+
+                                System.out.println("Stato aggiornato");
+
+                            break;
+                        case 2:
+
+                                System.out.println("Dimmi stato del progetto (IN SVILUPPO , IN TEST, PUBLICATO)");
+                                nuovostato=mystr.nextLine();
+                                nuovostato= team1.gioco.stato_del_progetto;
+
+                                System.out.println("Stato aggiornato");
+
+                            break;
+                        case 3:
+
+                                System.out.println("Dimmi stato del progetto (IN SVILUPPO , IN TEST, PUBLICATO)");
+                                nuovostato=mystr.nextLine();
+                                nuovostato= team1.gioco.stato_del_progetto;
+
+                                System.out.println("Stato aggiornato");
+
+                            break;
+                        default:
+                            System.out.println("Team non trovato");
+                    }
+                    
+
                    
 
                 case 5:
-                    
+
+                        System.out.println("Qui non esiste niente");
+                        break;
 
                 case 6:
                     System.out.println("Arrivederci!");
@@ -107,7 +210,7 @@ public class Sviluppo_videoludico {
             this.stato_del_progetto =stato_del_progetto;
         }
 
-        void stampaProdotto() {
+        void stampaGioco() {
             System.out.println("Titolo: " + titolo);
             System.out.println("Genere: "+ genere);
             System.out.println("Prezzo: " + costo_sviluppo);
@@ -122,11 +225,17 @@ public class Sviluppo_videoludico {
 
         Team(String nome_team) 
         {
+            this.nome_sviluppatori=nome_sviluppatori;
             this.nome_Team = nome_team;
         }
 
-        void stampaNegozio() {
-            System.out.println("Nome Team: " + nome_Team + "Nome sviluppatori: " + nome_sviluppatori);
+        void stampaTeam() {
+            System.out.println("Nome Team: " + nome_Team + "\n"+
+                                "Nome Sviluppatore"+ nome_sviluppatori + "\n"+
+                                "Titolo: " + gioco.titolo + "\n"+
+                                " Genere: " + gioco.genere + "\n"+
+                                "Prezzo per sviluppo: "+gioco.costo_sviluppo + "\n"+
+                                 "Stato del progetto: "+gioco.stato_del_progetto);
             System.out.println("--------------------------------------------");
             
         }
