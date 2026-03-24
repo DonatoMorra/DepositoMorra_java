40c14ffb6869d69400bf14ef298f4640fa951619
DonatoMorra <donatomorra90@gmail.com>
2026-03-24 17:03:51 +0100

    Update Sviluppo_videoludico.java

--- Corso_it/24marzo/pomeriggio/Sviluppo_videoludico.java
+++ Corso_it/24marzo/pomeriggio/Sviluppo_videoludico.java
@@ -7,19 +7,19 @@ public class Sviluppo_videoludico {
          Scanner mystr = new Scanner(System.in);
 
         
-        Negozio neg1 = new Negozio("Negozio 1");
-        Negozio neg2 = new Negozio("Negozio 2");
-        Negozio neg3 = new Negozio("Negozio 3");
+        Team team1 = new Team("Team 1");
+        Team team2 = new Team("Team 2");
+        Team team3  = new Team("Team 3");
 
         boolean continua = true;
 
         while (continua) {
             System.out.println("---Menu---");
-            System.out.println("1 - Inserisci Prodotto negozio");
-            System.out.println("2 - Visualizza dati di tutti i negozi");
-            System.out.println("3 - Cerca il negozio con il prodotto più costoso");
-            System.out.println("4 - Modifica quantità di un prodotto");
-            System.out.println("5 - Vendi prodotti da un negozio");
+            System.out.println("1 - Assegna gioco a un team");
+            System.out.println("2 - Visualizza tutti i team");
+            System.out.println("3 - Cerca il gioco con costo di sviluppo alto");
+            System.out.println("4 - Modifica stato di un gioco");
+            System.out.println("5 - aggiungi sviluppatori a un team");
             System.out.println("6 - Esci");
 
             System.out.print("Scegli opzione: ");
@@ -29,43 +29,46 @@ public class Sviluppo_videoludico {
             switch (op) 
             {
                 case 1:
-                    System.out.print("Scegli il negozio (1-3): ");
-                    int neg = myint.nextInt();
+                    System.out.print("Scegli il team (1-3): ");
+                    int tem = myint.nextInt();
                     
 
-                    System.out.print("Inserisci nome prodotto: ");
-                    String nome = mystr.nextLine();
+                    System.out.print("Inserisci Titolo: ");
+                    String titolo = mystr.nextLine();
 
-                    System.out.print("Inserisci prezzo: ");
-                    double prezzo = myint.nextDouble();
+                    System.out.print("Inserisci genere: ");
+                    String genere = mystr.nextLine();
 
-                    System.out.print("Inserisci quantità disponibile: ");
-                    int qta = myint.nextInt();
+                    System.out.print("Inserisci costo sviluppo: ");
+                    int csv = myint.nextInt();
+
+                    System.out.println("Dimmi stato del progetto");
+                    String stp=mystr.nextLine();
                     
 
-                    Prodotto newProdotto = new Prodotto(nome, prezzo, qta);
+                    Gioco newGioco = new Gioco(titolo, genere, csv, stp);
 
-                    switch (neg) {
-                        case 1: neg1.prodotto = newProdotto;
+                    switch (tem) {
+                        case 1: team1.gioco = newGioco;
                                 break;
 
-                        case 2: neg2.prodotto = newProdotto;
+                        case 2: team2.gioco = newGioco;
                                   break;
 
-                        case 3:  neg3.prodotto = newProdotto;
+                        case 3:  team3.gioco = newGioco;
                                  break;
 
-                        default: System.out.println("Negozio non esistente");
+                        default: System.out.println("Team non esistente");
                         
                     }
 
-                    System.out.println("Prodotto inserito ");
+                    System.out.println("Dati inseriti ");
                     break;
 
                 case 2:
-                    neg1.stampaNegozio();
-                    neg2.stampaNegozio();
-                    neg3.stampaNegozio();
+                    team1.stampaNegozio();
+                    team2.stampaNegozio();
+                    team3.stampaNegozio();
                     break;
 
                 case 3:
@@ -97,31 +100,33 @@ public class Sviluppo_videoludico {
         double costo_sviluppo;
         String stato_del_progetto;
 
-        Gioco(String titolo,String genere, double costo_sviluppo, String stato_del_prodotto) {
+        Gioco(String titolo,String genere, double costo_sviluppo, String stato_del_progetto) {
             this.titolo = titolo;
             this.genere = genere;
             this.costo_sviluppo = costo_sviluppo;
-            this.stato_del_prodotto =stato_del_prodotto;
+            this.stato_del_progetto =stato_del_progetto;
         }
 
         void stampaProdotto() {
-            System.out.println("Nome Prodotto: " + nome);
-            System.out.println("Prezzo: " + prezzo);
-            System.out.println("Quantità disponibile: " + quantita_disponibile);
+            System.out.println("Titolo: " + titolo);
+            System.out.println("Genere: "+ genere);
+            System.out.println("Prezzo: " + costo_sviluppo);
+            System.out.println("Stato del progetto: " + stato_del_progetto);
         }
     }
 
-    static class Negozio {
-        String nome_negozio;
-        Prodotto prodotto;
+    static class Team {
+        String nome_Team;
+        String nome_sviluppatori;
+        Gioco gioco;
 
-        Negozio(String nome_negozio) 
+        Team(String nome_team) 
         {
-            this.nome_negozio = nome_negozio;
+            this.nome_Team = nome_team;
         }
 
         void stampaNegozio() {
-            System.out.println("Negozio: " + nome_negozio + "Prodotti:\n " + "Nome: "+prodotto.nome +" Prezzo: "+ prodotto.prezzo + "Quantita: " + prodotto.quantita_disponibile);
+            System.out.println("Nome Team: " + nome_Team + "Nome sviluppatori: " + nome_sviluppatori);
             System.out.println("--------------------------------------------");
             
         }
